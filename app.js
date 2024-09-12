0c126addc29c0184c225d1bce7082defcf094bc3
Joel Latimer <joellatimer@outlook.com>
2024-09-12 12:31:24 -0400

    restore original

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const {engine} = require("express-handlebars")
 const bodyParser = require("body-parser") 
-const mysql = require('mysql2')
+const mysql = require('mysql')
 
 
 require("dotenv").config()
